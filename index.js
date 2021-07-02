e7a37da76491729f09a59a4b5156c6dc43c192c8
joseph <josephtkao@gmail.com>
2021-07-02 16:10:01 -0500

    updated lab to pass tests

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let timeInt = parseInt(time)
+  if (timeInt < 12) {
+    let greeting = 'Good Morning'
+    return greeting 
+  }else if (timeInt > 12 && timeInt < 17 ){
+    let greeting = 'Good Afternoon'
+    return greeting
+  }else{
+    let greeting = 'Good Evening'
+    return greeting
+  }
+}
+
+function displayMessage(message){
+  let greeting = document.querySelector('#greeting')
+  greeting.innerHTML = message
+}
 /* Write your implementation of displayMessage() */
